913073ad8dd33ac87a8f73161d8d905730ed94cb
No-more-pain <80432580+No-more-pain@users.noreply.github.com>
2021-06-17 21:54:19 +0200

    Onboarding

--- lib/Onboarding/OnBoarding.dart
+++ lib/Onboarding/OnBoarding.dart
@@ -11,11 +11,20 @@ class _OnBoardingState extends State<OnBoarding> {
   int currentindex = 0;
   List<OnBoardingModell> screens = [
     OnBoardingModell(
-        title: 'Messages', image: 'Assets/images/chat.webp', description: 'which allows designers to consider the form of a webpage or publication, '),
+        title: 'Messages',
+        image: 'Assets/images/chat.webp',
+        description:
+            'which allows designers to consider the form of a webpage or publication, '),
     OnBoardingModell(
-        title: 'Ideas', image: 'Assets/images/chat-1.webp', description: 'which allows designers to consider the form of a webpage or publication, '),
+        title: 'Ideas',
+        image: 'Assets/images/chat-1.webp',
+        description:
+            'which allows designers to consider the form of a webpage or publication, '),
     OnBoardingModell(
-        title: 'Planing', image: 'Assets/images/chat-2.webp', description: 'which allows designers to consider the form of a webpage or publication, '),
+        title: 'Planing',
+        image: 'Assets/images/chat-2.webp',
+        description:
+            'which allows designers to consider the form of a webpage or publication, '),
   ];
 
   @override
@@ -26,7 +35,6 @@ class _OnBoardingState extends State<OnBoarding> {
         children: [
           Flexible(
             child: Container(
-              
               child: PageView.builder(
                 onPageChanged: (index) {
                   setState(() {
@@ -36,15 +44,13 @@ class _OnBoardingState extends State<OnBoarding> {
                 itemCount: screens.length,
                 itemBuilder: (BuildContext context, int position) {
                   return Container(
-                    
-
                       child: SingleOnboardingPage(screens[position]));
                 },
               ),
             ),
           ),
           Transform.translate(
-            offset: Offset(100,100),
+            offset: Offset(100, 100),
             child: Row(
               children: _drawdots(screens.length),
             ),
@@ -61,22 +67,20 @@ class _OnBoardingState extends State<OnBoarding> {
     List<Widget> dots = [];
 
     for (int i = 0; i < qty; i++) {
-      dots.add(
-        Container(
-          margin: EdgeInsets.only(left: 30),
-            width: (i == currentindex ? 40 : 10),
-            height: (i == currentindex )? 10 : 10,
-            decoration: BoxDecoration(
-                borderRadius: (i== currentindex ) ? BorderRadius.circular(20):BorderRadius.circular(40),
-                color: (i == currentindex) ? Color.fromRGBO(26,183, 252, 1) : Color.fromRGBO(147, 166, 170, 1),
-                 
-
-            
-           ),
-        )
-      );
+      dots.add(Container(
+        margin: EdgeInsets.only(left: 30),
+        width: (i == currentindex ? 40 : 10),
+        height: (i == currentindex) ? 10 : 10,
+        decoration: BoxDecoration(
+          borderRadius: (i == currentindex)
+              ? BorderRadius.circular(20)
+              : BorderRadius.circular(40),
+          color: (i == currentindex)
+              ? Color.fromRGBO(26, 183, 252, 1)
+              : Color.fromRGBO(147, 166, 170, 1),
+        ),
+      ));
     }
     return dots;
-    
   }
 }
